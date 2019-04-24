30b4d7351c6bcd834306b7445b3576bae00c4127
Cookie_Lover22 <daniel.boon@student.hu.nl>
2019-04-24 12:09:09 +0200

    lelijke vierkant code

--- Coocox project/main.c
+++ Coocox project/main.c
@@ -13,6 +13,7 @@
 #include "main.h"
 #include "stm32_ub_vga_screen.h"
 #include <math.h>
+#include <time.h>
 
 int main(void)
 {
@@ -24,14 +25,39 @@ int main(void)
 	UB_VGA_Screen_Init(); // Init VGA-Screen
 
   UB_VGA_FillScreen(VGA_COL_BLACK);
-  UB_VGA_SetPixel(10,10,10);
-  UB_VGA_SetPixel(100,100,10);
-  UB_VGA_SetPixel(200,200,10);
-
-
+  //UB_VGA_SetPixel(10,10,10);
+  //UB_VGA_SetPixel(100,100,10);
+  //srand (time(NULL));
+  int getalX = rand() % VGA_DISPLAY_X;
+  int getalY = rand() % VGA_DISPLAY_Y;
+  int i=0;
+  int j=0;
+  int startpunt[] = {10,100};
+  int eindpunt[] = {100,100};
+  int breedte = 100;
+  int kleur = 10;
+  for(i=startpunt[0];i<eindpunt[0];i++)
+  {
+	  for(j=0;j<breedte;j++)
+	  {
+		  UB_VGA_SetPixel(j+startpunt[1],i,kleur);
+	  }
+  }
   while(1)
   {
-	  // put the code here
+
+	  /*getalX = rand() % VGA_DISPLAY_X;
+	  getalY = rand() % VGA_DISPLAY_Y;
+	  UB_VGA_SetPixel(getalX,getalY,10);
+	  while(1)
+	  {
+		  i++;
+		  if(i>1000)
+		  {
+			  i=0;
+			  break;
+		  }
+	  }*/
+
   }
 }
-
