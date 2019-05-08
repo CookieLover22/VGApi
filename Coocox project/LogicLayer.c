d2bc38e5f5c04368c457745a6076d58e2413de3d
kaya <kayahartwig@gmail.com>
2019-05-08 12:51:10 +0200

    Errorhandling toegevoegd, Qwrite en Qinit uitgeschreven

---
+++ Coocox project/LogicLayer.c
@@ -0,0 +1,63 @@
+#include <main.h>
+#include <LogicLayer.h>
+
+/*! Deze functie genereert de Queue die gebruikt kan worden.
+ *  Het aantal leden van deze Queue wordt meegegeven en de
+ *  functie geeft het adres van de Queue terug.
+ */
+UART_Q_info * API_Qinit(int Qsize, int * error)
+{
+	if(Qsize < 1) {*error = 1; return QSIZEERROR;}
+	UART_Q_info initQ;
+	UART_command initCommand[Qlength];
+
+	initQ.Q_size = Qlength;
+	initQ.last_written_Q_member = 0;
+	initQ.last_read_Q_member = 0;
+	initQ.Q_address = &initCommand;
+
+	*error = NOERROR;
+	return &initQ;
+}
+
+/*!Deze functie schrijft een nieuw lid in de Q en returnt een errorcode.
+ * Het adres van de Q (van API_Qinit()) wordt meegegeven aan de functie.
+ * Ook wordt de UART_command struct die in de Q moet komen meegegeven
+ */
+int API_Qwriter(UART_Q_info * writeQ, UART_command * writeCommand)
+{
+	if (writeQ->last_written_Q_member < writeQ->Q_size - 1) writeQ->last_written_Q_member++;
+	else writeQ->last_written_Q_member = 0;
+
+	writeQ->Q_members[writeQ->last_written_Q_member];
+
+	return NOERROR;
+
+}
+
+/*!Deze functie leest het oudste lid naar de Q en returnt een errorcode.
+ * Het adres van de Q (van API_Qinit()) wordt meegegeven aan de functie.
+ */
+int API_Qreader(UART_Q_info *, UART_struct *)
+{
+
+}
+
+int API_functionpicker()
+{
+	switch(expression)
+	{
+
+	   case constant-expression  :
+	      statement(s);
+	      break; /* optional */
+
+	   case constant-expression  :
+	      statement(s);
+	      break; /* optional */
+
+	   /* you can have any number of case statements */
+	   default : /* Optional */
+	   statement(s);
+	}
+}
---
+++ Coocox project/LogicLayer.h
@@ -0,0 +1,37 @@
+//==========================
+//functieprototypes
+int API_Qinit(int Qsize);
+int API_Qwriter(UART_Q_info * writeQ, UART_command * writeCommand);
+int API_Qreader(UART_Q_info *, UART_struct *);
+
+
+//==========================
+//variabelen
+
+/*!Dit is de struct met info over de Queue. Deze wordt teruggegeven door de API_Qinit functie
+ *
+ */
+typedef struct Qinfo
+{
+	//! deze variabele geeft aan hoe lang de Q is.
+	int Q_size = 0;
+	//! dit is de variabele die aan geeft waar in de Q het nieuwste lid zit (++ bij schrijven Q)
+	int last_written_Q_member = 0;
+	/*! dit is de variabele die aan geeft waar in de Q het laatst gelezen lid zit (++ bij lezen Q).
+	 *  Als deze gelijk is aan first_in_Q_member is de Q leeg.
+	 */
+	int last_read_Q_member = 0;
+
+	UART_command* Q_members;
+
+} UART_Q_info;
+
+/*!Dit is de uart struct. De UART Queue die de front en de logic layer verbind
+ * bevat structs van dit type
+ */
+typedef struct command
+{
+	char arg[8][20];
+
+} UART_command;
+
