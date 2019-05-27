4764a4ec0363864d32daef4742e6db9769475c61
Mauricio Paulusma <Mauricio@hotmail.nl>
2019-05-27 17:15:29 +0200

    doxygen.c added

---
+++ Coocox project/doxygen.c
@@ -0,0 +1,8 @@
+/*! @mainpage VGApi
+ *
+ * Dit is de hoofdpagina van het VGApi project voor het vak Software Ontwikkeling. Ga naar de files tab om de documentatie van de files te bekijken.
+ *
+ * Veel leesplezier! :)
+ *
+ *
+ */
